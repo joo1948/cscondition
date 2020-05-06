eae4996b7d948f0fe7cecb849476f905e5bc0f4d
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2020-05-06 15:17:01 +0900

    #4 13-6. 삼항 연산자 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -22,6 +22,7 @@ namespace cscondition
                 Console.WriteLine("홀수입니다.!");
             }
             */
+            /*
             switch(input % 2)
             {
                 case 0:
@@ -32,7 +33,10 @@ namespace cscondition
                     break;
                 
             }
-
+            */
+            Console.WriteLine(input % 2 ==0 ? "짝수입니다." : "홀수입니다");
+            Console.WriteLine(input > 0 ? "자연수입니다" : "자연수가 아닙니다");
+            
             //현재시간 구하기
             Console.WriteLine(DateTime.Now.Year);
             Console.WriteLine(DateTime.Now.Month);
