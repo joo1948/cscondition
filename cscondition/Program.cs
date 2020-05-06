bb7306b0b1a4ddda6632af22a3aa465304cee6e4
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2020-05-06 14:39:10 +0900

    13-2. if else 조건문 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -22,6 +22,23 @@ namespace cscondition
                 Console.WriteLine("홀수입니다.!");
             }
 
+            //현재시간 구하기
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+
+            //오전과 오후 구분
+            if (DateTime.Now.Hour < 12)
+            {
+                Console.WriteLine("오전입니다.");
+            }
+            else
+            {
+                Console.WriteLine("오후입니다.");
+            }
         }
     }
 }
